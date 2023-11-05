b18184f622f759e1e43500ee2ad7ef34fd3db138
Ranjan kumar <136085563+MrRanjan12@users.noreply.github.com>
2023-11-05 11:59:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -8,19 +8,19 @@ document.querySelectorAll("button")[i].addEventListener("click",function(){
    var buttonInnerHtml=this.innerHTML;
  switch (this.innerHTML) {
   case "w":
-    var audio = new Audio('sounds/Tom-1.mp3');
+    var audio = new Audio('sounds/tom-1.mp3');
     audio.play();
     break;
   case "a":
-    var audio = new Audio('sounds/Tom-2.mp3');
+    var audio = new Audio('sounds/tom-2.mp3');
     audio.play();
   break;
   case "s":
-    var audio = new Audio('sounds/Tom-3.mp3');
+    var audio = new Audio('sounds/tom-3.mp3');
     audio.play();
   break;
   case "d":
-    var audio = new Audio('sounds/Tom-4.mp3');
+    var audio = new Audio('sounds/tom-4.mp3');
     audio.play();
   break;
   case "j":
