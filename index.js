b648540fb6d1a51ec1d022dda7e54ae35c7192cb
Ranjan kumar <136085563+MrRanjan12@users.noreply.github.com>
2023-11-07 08:00:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//detecting button press;
+
 var numberOfDrrumbutton=document.querySelectorAll(".drum").length;
 
 for(var i=0;i<numberOfDrrumbutton;i++)
@@ -6,43 +8,52 @@ document.querySelectorAll("button")[i].addEventListener("click",function(){
    // alert("i have got clicked");
     //what to do when click is detected;
    var buttonInnerHtml=this.innerHTML;
- switch (this.innerHTML) {
-  case "w":
-    var audio = new Audio('sounds/tom-1.mp3');
-    audio.play();
-    break;
-  case "a":
-    var audio = new Audio('sounds/tom-2.mp3');
-    audio.play();
-  break;
-  case "s":
-    var audio = new Audio('sounds/tom-3.mp3');
-    audio.play();
-  break;
-  case "d":
-    var audio = new Audio('sounds/tom-4.mp3');
-    audio.play();
-  break;
-  case "j":
-    var audio = new Audio('sounds/snare.mp3');
-    audio.play();
-  break;
-  case "k":
-    var audio = new Audio('sounds/crash.mp3');
-    audio.play();
-  break;
-  case "l":
-    var audio = new Audio('sounds/kick-bass.mp3');
-    audio.play();
-  break;
-  default: console.log(buttonInnerHtml);
-    
- }
+   makesound(this.innerHTML);
    
 });
 }
 
+//Detecting keyword press;
 
+document.addEventListener("keydown",function(event){
+    makesound(event.key);
+});
+
+function makesound(key){
+
+  switch (key) {
+    case "w":
+      var audio = new Audio('sounds/Tom-1.mp3');
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio('sounds/Tom-2.mp3');
+      audio.play();
+    break;
+    case "s":
+      var audio = new Audio('sounds/Tom-3.mp3');
+      audio.play();
+    break;
+    case "d":
+      var audio = new Audio('sounds/Tom-4.mp3');
+      audio.play();
+    break;
+    case "j":
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+    break;
+    case "k":
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+    break;
+    case "l":
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+    break;
+    default: console.log(buttonInnerHtml);
+       
+   }
+}
 /*   var audio = new Audio('sounds/crash.mp3');
   audio.play();  
   
